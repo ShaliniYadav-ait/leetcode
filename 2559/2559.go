1bdd7c8c66342045318f057cff068bdd68405f64
Shalini Yadav <shaliniyadav.ait@gmail.com>
2025-02-16 17:09:52 +0530

    2559

---
+++ 2559/2559.go
@@ -0,0 +1,47 @@
+package main
+
+type ProductOfNumbers struct {
+	Nums []int
+	Zero map[int][]int
+}
+
+func Constructor() ProductOfNumbers {
+	return ProductOfNumbers{
+		Nums: []int{},
+		Zero: make(map[int][]int),
+	}
+}
+
+func (this *ProductOfNumbers) Add(num int) {
+	if num == 0 {
+		this.Zero[0] = append(this.Zero[0], len(this.Nums))
+		this.Nums = append(this.Nums, num)
+		return
+	}
+	lastProd := 1
+	if len(this.Nums) > 0 {
+		lastProd = this.Nums[len(this.Nums)-1]
+		if lastProd == 0 {
+			lastProd = 1
+		}
+	}
+	currProd := lastProd * num
+	this.Nums = append(this.Nums, currProd)
+}
+
+func (this *ProductOfNumbers) GetProduct(k int) int {
+	last := len(this.Nums) - 1
+	first := last - k + 1
+	if val, ok := this.Zero[0]; ok {
+		for i := 0; i < len(val); i++ {
+			if val[i] >= this.Nums[first] && val[i] <= this.Nums[last] {
+				return 0
+			}
+		}
+	}
+	var prodK int
+	if first-1 >= 0 && this.Nums[first-1] > 0 {
+		prodK = this.Nums[last] / this.Nums[first-1]
+	}
+	return prodK
+}
---
+++ 2559/2559_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+var zeroMap = make(map[int][]int)
+func TestProductOfNumbers_Add(t *testing.T) {
+	type fields struct {
+		Nums []int
+		Zero map[int][]int
+	}
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+
+		{
+			name: "add number 3",
+			fields: fields{
+				Nums: []int{1, 2},
+				Zero: zeroMap,
+			},
+			args: args{
+				num: 3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := &ProductOfNumbers{
+				Nums: tt.fields.Nums,
+			}
+			this.Add(tt.args.num)
+			fmt.Println(tt.fields.Nums)
+		})
+	}
+}
+
+func TestProductOfNumbers_GetProduct(t *testing.T) {
+	zeroMap[0] = append(zeroMap[0], 1)
+	type fields struct {
+		Nums []int
+		Zero map[int][]int
+	}
+	type args struct {
+		k int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "get product",
+			fields: fields{
+				Nums: []int{1, 2, 6},
+				Zero : zeroMap,
+			},
+			args: args{
+				k: 2,
+			},
+			want: 6,
+		},
+		{
+			name: "get product 2nd",
+			fields: fields{
+				Nums: []int{3,0,2,10,40,320},
+				Zero : zeroMap,
+			},
+			args: args{
+				k: 3,
+			},
+			want: 40,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := &ProductOfNumbers{
+				Nums: tt.fields.Nums,
+			}
+			if got := this.GetProduct(tt.args.k); got != tt.want {
+				t.Errorf("ProductOfNumbers.GetProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+// ["ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"]
+// [[],[,[2],[3],[4],,[2]]
+
+
