8164b1ed1dd6f2f3bb8bbca477d55659aebb9a89
Shalini Yadav <shaliniyadav.ait@gmail.com>
2025-02-16 21:12:19 +0530

    2270 test case

---
+++ 2270/2270_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+
+func Test_waysToSplitArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "first",
+			args: args{
+				nums: []int{10, 4, -8, 7},
+			},
+			want: 2,
+		},
+		{
+			name: "second",
+			args: args{
+				nums: []int{2,3,1,0},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := waysToSplitArray(tt.args.nums); got != tt.want {
+				t.Errorf("waysToSplitArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// {
+// 	name : ,
+// args : args{
+// 	nums : []int{},
+// },
+// want : ,
+// }
