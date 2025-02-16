57bba80693b5d012c2823dc3bcc036b19d3f456e
Shalini Yadav <shaliniyadav.ait@gmail.com>
2025-02-16 17:08:46 +0530

    1718 solution

--- 1718/1718.go
+++ 1718/1718.go
@@ -1,6 +1,42 @@
 package main
 
 func constructDistancedSequence(n int) []int {
+	size := n*2 - 1
+	result := make([]int, size)
+	isUsed := make([]bool, n+1)
 
-	return []int{}
+	backtracking(0, result, isUsed, n)
+	return result
+}
+
+func backtracking(index int, result []int, isUsed []bool, n int) bool {
+	if index == len(result) {
+		return true
+	}
+
+	for num := n; num >= 1; num-- {
+		if isUsed[num] || (num > 1 && (num+index >= len(result)) || (result[num+index] > 0)) {
+			continue
+		}
+
+		isUsed[num] = true
+		result[index] = num
+		if num > 1 {
+			result[index+num] = num
+		}
+		j := index + 1
+		for j < len(result) && result[j] > 0 {
+			j++
+		}
+		if backtracking(j, result, isUsed, n) {
+			return true
+		}
+
+		isUsed[num] = false
+		result[index] = 0
+		if num > 1 {
+			result[index+num] = 0
+		}
+	}
+	return false
 }
