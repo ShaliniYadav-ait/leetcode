ce982bd92b9a2346ce702092b812cb6ca4fdc1b0
Shalini Yadav <shaliniyadav.ait@gmail.com>
2025-02-16 21:17:30 +0530

    03 Jan 2025

--- 2270/2270.go
+++ 2270/2270.go
@@ -1,5 +1,23 @@
 package main
 
 func waysToSplitArray(nums []int) int {
-    return 0
+	totalSum := getSum(nums)
+	currSum := 0
+	count := 0
+	for i := 0; i < len(nums)-1; i++ {
+		currSum += nums[i]
+		sumRight := totalSum - currSum
+		if currSum >= sumRight {
+			count++
+		}
+	}
+	return count
+}
+
+func getSum(nums []int) int {
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+	return sum
 }
