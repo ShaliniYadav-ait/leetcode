c5ccecd30551b43b3f26e641dee97cb6b767111b
Shalini Yadav <shaliniyadav.ait@gmail.com>
2025-02-17 08:01:30 +0530

    17 Feb 2025

--- 1079/1079.go
+++ 1079/1079.go
@@ -1,7 +1,29 @@
 package main
 
 func numTilePossibilities(tiles string) int {
-    // getMap := getCharactersMap(tiles)
-	return 0
+    mapChar := getCharactersMap(tiles)
+	
+	return getTilePossibilities(mapChar)
 }
 
+func getTilePossibilities(mapChar map[byte]int)int{
+	count := 0
+
+	for key,val := range mapChar{
+		if val > 0{
+			count++
+			mapChar[key]--
+			count += getTilePossibilities(mapChar)
+			mapChar[key]++
+		}
+	}
+	return count
+}
+
+func getCharactersMap(tiles string)map[byte]int{
+	mapChar := make(map[byte]int)
+	for i := 0; i < len(tiles); i++{
+		mapChar[tiles[i]]++
+	}
+	return mapChar
+}
