fff7785995bbf2531f0f8f60d356d953a502ff0a
ShaliniYadav-ait <70330551+ShaliniYadav-ait@users.noreply.github.com>
2025-02-17 12:53:16 +0000

    2nd problem

---
+++ 2/2.go
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+
+ package main
+
+ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    return &ListNode{}
+ }
